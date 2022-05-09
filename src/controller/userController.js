db9694d9cb4acc6963a1fa2b3e5c552d4c311a22
Karan039 <krndhamija9@gmail.com>
2022-05-09 17:59:38 +0530

    adfs

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,9 +1,19 @@
 const userModel = require("../models/usermodel")
 
 const registerUser = async function(req, res){
-    if(Object.keys(req.body).length == 0) return res.status(400).send({status : false, message : "Oops, you forgot to fill data inside request body"})
-
+    
     let data = req.body
+
+    function isPresent(value){
+        if(!value && value.trim().length==0)
+        return true;
+    }
+
+    function badRequest(){
+        if(Object.keys(data).length == 0) 
+    return res.status(400).send({status : false, message : "Oops, you forgot to fill data inside request body"})
+        
+    }
 
     let created = await userModel.create(data)
 
