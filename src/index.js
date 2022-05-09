bb0e5b5ceb051e190a9611f860156b4b183b435e
Mohammad Naseem <niceman1915@gmail.com>
2022-05-09 16:34:43 +0530

    INITIAL

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-Parser');
+const route = require('./routes/routes.js')
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://nas:nas1234@cluster0.fci9p.mongodb.net/group28Database", {
+    useNewUrlParser: true
+})
+
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+
+
+app.use('/', route);
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
