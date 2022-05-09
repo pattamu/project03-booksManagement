5f7915c3b58b88f805896a97d8fecf7fb5477632
Mohammad Naseem <niceman1915@gmail.com>
2022-05-09 17:43:01 +0530

    added valdations in usercontr

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,13 +1,30 @@
 const userModel = require("../models/usermodel")
 
+const isValidParams = function(arr){
+    let params = ["name", "title", "phone", "email", "password"]
+    for(let keys of arr){
+        if(params.indexOf(keys) == -1) return false
+    }
+    if(arr.length == 6){
+        if(arr.indexOf("address") == -1) return false
+    }
+    return true
+}
+
 const registerUser = async function(req, res){
-    if(Object.keys(req.body).length == 0) return res.status(400).send({status : false, message : "Oops, you forgot to fill data inside request body"})
+    try{
+        if(!isValidParams(Object.keys(req.body))) return res.status(400).send({status : false, message : "Invalid Params, there might be something that should not be there"})
 
-    let data = req.body
+        if(Object.keys(req.body).length == 0) return res.status(400).send({status : false, message : "Oops, you forgot to fill data inside request body"})
 
-    let created = await userModel.create(data)
+        let data = req.body
 
-    res.status(201).send({status : true, message : "Success", data : created})
+        let created = await userModel.create(data)
+
+        res.status(201).send({status : true, message : "Success", data : created})
+    }catch(error){
+        res.status(500).send({status : false, message : error.message})
+    }
 }
 
 module.exports.registerUser = registerUser
