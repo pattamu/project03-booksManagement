05014513ead012641f1c184013094bc0c7e71b13
pattamu <sandeepkumar416166@gmail.com>
2022-05-10 14:43:28 +0530

    updateBook api added

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -91,7 +91,7 @@ const createBook = async function (req, res) {
 const getBooks = async function (req, res) {
     try {
         if (Object.keys(req.query).length == 0) {
-            let data = await bookController.find({ isDeleted: false }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
+            let data = await bookModel.find({ isDeleted: false }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
             if (data.length == 0) 
             return res.status(404).send({ status: false, message: "No books exists" })
 
@@ -107,8 +107,8 @@ const getBooks = async function (req, res) {
             let category = req.query.category
             let subcategory = req.query.subcategory
 
-            let allBooks = await bookController.find({ $and: [{ $or: [{ userid: userId }, { category: category }, { subcategory: subcategory }] }, { isDeleted: false }] })
-
+            let allBooks = await bookModel.find({ $and: [{ $or: [{ userid: userId }, { category: category }, { subcategory: subcategory }] }, { isDeleted: false }] })
+                                        .select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
             if (allBooks.length == 0) return res.status(400).send({ status: false, message: "No books with selected query params" })
 
             let sortedBooks = allBooks.sort((a, b) => {
@@ -126,6 +126,63 @@ const getBooks = async function (req, res) {
         res.status(500).send({ status: false, message: error.message })
     }
 }
+
+
+//Update Book API Function
+const updateBook = async (req,res) => {
+    let data = req.body
+    let bookId = req.params.bookId
+    let authUser = req.headers['valid-auth-user_id']
+    let titleRegEx = /^[,.-_ a-zA-Z0-9]+$/
+    let ISBN_RegEx = /^(?:ISBN(?:-1[03])?:?●)?(?=[0-9X]{10}$|(?=(?:[0-9]+[-●]){3})[-●0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[-●]){4})[-●0-9]{17}$)(?:97[89][-●]?)?[0-9]{1,5}[-●]?[0-9]+[-●]?[0-9]+[-●]?[0-9X]$/
+    
+    const isValid = function (value) {
+        if (typeof value === 'undefined' || value === null) return false
+        if (typeof value === 'string' && value.trim().length === 0) return false
+        return true;
+    }
+
+    try{
+        if(!mongoose.isValidObjectId(bookId))
+            return res.status(400).send({status: false, message: "Please enter a Valid Book ObjectId."}) 
+
+        let findBook = await bookModel.findById(bookId)
+        if(!findBook)
+            return res.status(404).send({status: false, message: "There is No Book available with this bookId."})
+
+        if(findBook.userId != authUser)
+            return res.status(401).send({status: false, message: "You don't have Authority to Update this Book."})
+        
+        if(!Object.keys(data).length)
+            return res.status(400).send({status: false, message: "Please Enter Data. Book can't be Updated without any Data."})
+
+        if(data.title && !isValid(data.title))
+            return res.status(400).send({status: false, message: "Please Enter some data in Title."})
+
+        if(data.title && !titleRegEx.test(data.title.trim()))
+            return res.status(400).send({status: false, message: "Please Enter a valid Title for your Book."})
+
+        if(data.ISBN && !ISBN_RegEx.test(data.ISBN.trim()))
+            return res.status(400).send({status: false, message: "Please Enter a valid ISBN for your Book."})
+        
+        let updateBook = await bookModel.findOneAndUpdate({
+            _id: findBook, isDeleted: false},{
+                title: req.body.title,
+                excerpt: req.body.excerpt,
+                releasedAt: data.releasedAt,
+                ISBN: req.body.ISBN},{
+                    new:true
+            })
+        if(!updateBook)
+            return res.status(404).send({status: false, message: "Book Not Found."})
+
+        res.status(200).send({status: true, message: 'Success', data: updateBook })
+    }catch(err){
+        console.log(err.message)
+        res.status(500).send({ status:false, message: err.message})
+    }
+}
+
 
 //        Delete Api
 const deleteBooksBYId = async function (req, res) {
@@ -151,4 +208,4 @@ const deleteBooksBYId = async function (req, res) {
     }
 }
 
-module.exports = { createBook, deleteBooksBYId, getBooks }
+module.exports = { createBook, deleteBooksBYId, getBooks, updateBook }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const { registerUser } = require("../controller/userController")
 const { userLogin } = require("../controller/loginController")
 const { userAuthentication } = require("../middleware/authentication")
-const { createBook, getBooks, deleteBooksBYId } = require("../controller/bookController")
+const { createBook, getBooks, updateBook, deleteBooksBYId } = require("../controller/bookController")
 
 router.post("/register", registerUser)
 router.post("/login", userLogin)
 router.post("/books", createBook)
 
 router.get("/books", getBooks)
+router.put("/books/:bookId", userAuthentication, updateBook)
 router.delete("/books/:bookId", deleteBooksBYId)
 
 
