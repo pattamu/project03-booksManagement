4526d27cff7f8b3e2f09a90b693a0245a4308fc2
Karan039 <krndhamija9@gmail.com>
2022-05-10 22:50:37 +0530

    update

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -92,8 +92,8 @@ const getBooks = async function (req, res) {
     try {
         if (Object.keys(req.query).length == 0) {
             let allBooks = await bookModel.find({ isDeleted: false }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
-            if (allBooks.length == 0) 
-            return res.status(404).send({ status: false, message: "No books exists" })
+            if (allBooks.length == 0)
+                return res.status(404).send({ status: false, message: "No books exists" })
             let sortedBooks = allBooks.sort((a, b) => {
                 let first = a.title.toUpperCase()
                 let second = b.title.toUpperCase()
@@ -112,10 +112,10 @@ const getBooks = async function (req, res) {
             let category = req.query.category
             let subcategory = req.query.subcategory
 
-            let allBooks = await bookModel.find({ $and: [{ $or: [{ userId: userId }, { category: category }, { subcategory: subcategory }] }, { isDeleted: false }] })
-                                        .select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
-            if (allBooks.length == 0) 
-            return res.status(400).send({ status: false, message: "No books with selected query params" })
+            let allBooks = await bookModel.find({ $and: [{ $or: [{ userId: userId }, { category: category }, { subcategory: subcategory }] }, { isDeleted: false }] }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).collation({ locale: "en", strength: 2 })
+
+            if (allBooks.length == 0)
+                return res.status(400).send({ status: false, message: "No books with selected query params" })
 
             let sortedBooks = allBooks.sort((a, b) => {
                 let first = a.title.toUpperCase()
@@ -125,7 +125,7 @@ const getBooks = async function (req, res) {
                 return 0
             })
             res.status(200).send({ status: true, message: "Success", data: sortedBooks })
-         }
+        }
     }
     catch (error) {
         res.status(500).send({ status: false, message: error.message })
@@ -134,57 +134,63 @@ const getBooks = async function (req, res) {
 
 
 //Update Book API Function
-const updateBook = async (req,res) => {
+const updateBook = async (req, res) => {
     let data = req.body
     let bookId = req.params.bookId
     let authUser = req.headers['valid-auth-user_id']
     let titleRegEx = /^[,.-_ a-zA-Z0-9]+$/
     let ISBN_RegEx = /^(?:ISBN(?:-1[03])?:?●)?(?=[0-9X]{10}$|(?=(?:[0-9]+[-●]){3})[-●0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[-●]){4})[-●0-9]{17}$)(?:97[89][-●]?)?[0-9]{1,5}[-●]?[0-9]+[-●]?[0-9]+[-●]?[0-9X]$/
-    
+
     const isValid = function (value) {
         if (typeof value === 'undefined' || value === null) return false
         if (typeof value === 'string' && value.trim().length === 0) return false
         return true;
     }
 
-    try{
-        if(!mongoose.isValidObjectId(bookId))
-            return res.status(400).send({status: false, message: "Please enter a Valid Book ObjectId."}) 
+    try {
+        if (!mongoose.isValidObjectId(bookId))
+            return res.status(400).send({ status: false, message: "Please enter a Valid Book ObjectId." })
 
         let findBook = await bookModel.findById(bookId)
-        if(!findBook)
-            return res.status(404).send({status: false, message: "There is No Book available with this bookId."})
+        if (!findBook)
+            return res.status(404).send({ status: false, message: "There is No Book available with this bookId." })
 
-        if(findBook.userId != authUser)
-            return res.status(401).send({status: false, message: "You don't have Authority to Update this Book."})
-        
-        if(!Object.keys(data).length)
-            return res.status(400).send({status: false, message: "Please Enter Data. Book can't be Updated without any Data."})
+        if (findBook.userId != authUser)
+            return res.status(401).send({ status: false, message: "You don't have Authority to Update this Book." })
 
-        if(data.title && !isValid(data.title))
-            return res.status(400).send({status: false, message: "Please Enter some data in Title."})
+        if (!Object.keys(data).length)
+            return res.status(400).send({ status: false, message: "Please Enter Data. Book can't be Updated without any Data." })
 
-        if(data.title && !titleRegEx.test(data.title.trim()))
-            return res.status(400).send({status: false, message: "Please Enter a valid Title for your Book."})
+        if (data.title && !isValid(data.title))
+            return res.status(400).send({ status: false, message: "Please Enter some data in Title." })
 
-        if(data.ISBN && !ISBN_RegEx.test(data.ISBN.trim()))
-            return res.status(400).send({status: false, message: "Please Enter a valid ISBN for your Book."})
-        
+        if (await bookModel.findOne({ title: data.title }))
+            return res.status(400).send({ status: false, message: "Book with same title is already present" })
+
+
+        if (data.title && !titleRegEx.test(data.title.trim()))
+            return res.status(400).send({ status: false, message: "Please Enter a valid Title for your Book." })
+
+        if (data.ISBN && !ISBN_RegEx.test(data.ISBN.trim()))
+            return res.status(400).send({ status: false, message: "Please Enter a valid ISBN for your Book." })
+
         let updateBook = await bookModel.findOneAndUpdate({
-            _id: findBook, isDeleted: false},{
-                title: req.body.title,
-                excerpt: req.body.excerpt,
-                releasedAt: data.releasedAt,
-                ISBN: req.body.ISBN},{
-                    new:true
-            })
-        if(!updateBook)
-            return res.status(404).send({status: false, message: "Book Not Found."})
+            _id: findBook, isDeleted: false
+        }, {
+            title: req.body.title,
+            excerpt: req.body.excerpt,
+            releasedAt: data.releasedAt,
+            ISBN: req.body.ISBN
+        }, {
+            new: true
+        })
+        if (!updateBook)
+            return res.status(404).send({ status: false, message: "Book Not Found." })
 
-        res.status(200).send({status: true, message: 'Success', data: updateBook })
-    }catch(err){
+        res.status(200).send({ status: true, message: 'Success', data: updateBook })
+    } catch (err) {
         console.log(err.message)
-        res.status(500).send({ status:false, message: err.message})
+        res.status(500).send({ status: false, message: err.message })
     }
 }
 
